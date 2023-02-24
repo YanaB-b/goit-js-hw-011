c440597bcb551852f204a5ab3baee74bf6e59c9f
YanaB-b <j.biliuk96@gmail.com>
2023-02-24 21:30:07 +0100

    +

---
+++ src/01-index.js
@@ -0,0 +1,110 @@
+import simpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from "notiflix";
+import GalleryApiService from "./NewsApiService";
+import LoadMoreBtn from "./components/loadMoreBtn";
+
+const ref = {
+  form: document.querySelector(".search-form"),
+  gallery: document.querySelector(".gallery"),
+
+}; 
+ 
+
+const lightbox = new simpleLightbox('.gallery a');
+const galleryApiService = new GalleryApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: ".load-more",
+  isHidden: true,
+});
+
+ref.form.addEventListener("submit", onSubmit);
+loadMoreBtn.button.addEventListener("click", fetchArticles);
+
+function onSubmit(e) {
+   e.preventDefault();
+   
+   galeryApiService.q = e.currentTarget.elements.searchQuery.value.trim();
+
+   loadMoreBtn.show();
+   galleryApiService.resetPage();
+   clearList();
+   fetchArticles();
+
+}
+function fetchArticles() {
+    loadMoreBtn.disable();
+    loadMoreBtn.show();
+
+    return galleryApiService
+      .getImagesy()
+      .then(res => {
+        notification(res);
+     
+       return ref.hits
+      })
+      .then(appendGallery)
+      .catch(OnError)
+      .finally(() => {
+        ref.form.reset();
+        loadMoreBtn.enable();
+
+      });
+}
+function appendGallery(markup) {
+    let img = '';
+    img = markup.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) =>
+    { return `
+    <a class="gallery-item" href="${largeImageURL}">
+    <div class="photo-card">
+  
+    <img class="images" src="${webformatURL}" alt="${tags}" loading="lazy" width="280" height="180"/>
+    
+<div class="info">
+  <p class="info-item"> <b>Likes</b>
+    <span>${likes}</span>
+  </p>
+  <p class="info-item"><b>Views</b> 
+    <span>${views}</span>
+  </p>
+  <p class="info-item">
+    <b>Comments</b>
+    <span>${comments}</span>
+  </p>
+  <p class="info-item">
+    <b>Downloads</b>
+    <span>${downloads}</span>
+  </p>
+</div>
+</div></a>`;
+    }
+    )
+    .join('');
+
+
+    ref.gallery.insertAdjacentHTML("beforeend", img);
+    lightbox.refresh();
+  }
+  
+  function clearList() {
+    ref.gallery.innerHTML = "";
+  }
+function OnError(err) {
+    console.log (err);
+  
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    loadMoreBtn.hide();
+}
+
+function notification(res) {
+  if (galleryApiService.page === 2)
+Notiflix.Notify.success(`Hooray! We found ${res.totalHits} images.`);
+else if (res.hits.length < galleryApiService.perPage) {
+  loadMoreBtn.hide();
+  Notiflix.Notify.warning(
+    "We're sorry, but you've reached the end of search results."
+  );
+}  
+}
+
---
+++ src/02-index.js
@@ -0,0 +1,116 @@
+import simpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from "notiflix";
+import GalleryApiService from "./NewsApiService";
+
+
+
+const ref = {
+    form: document.querySelector(".search-form"),
+    gallery: document.querySelector(".gallery"),
+    guard: document.querySelector('.guard'),
+  
+  };
+   
+  const lightbox = new simpleLightbox('.gallery a');
+  const galleryApiService = new GalleryApiService();
+
+  ref.form.addEventListener("submit", onSubmit);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    
+   galleryApiService.q = e.currentTarget.elements.searchQuery.value.trim();
+ 
+
+    galleryApiService.resetPage();
+    clearList();
+    fetchArticles();
+ 
+ }
+ function fetchArticles() {
+ 
+     return galleryApiService
+       .getImagesy()
+       .then(res => {
+        observer.observe(ref.guard);
+        notification(res);
+        return res.hits;
+       })
+       .then(appendGallery)
+       .catch(onError)
+       .finally(() => {
+         ref.form.reset();
+        
+ 
+       });
+ }
+
+ function appendGallery(markup) {
+    let img = '';
+    img = markup.map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads}) =>
+    { return `<a class="gallery-item" href="${largeImageURL}">
+    <div class="photo-card">
+  
+    <img class="images" src="${webformatURL}" alt="${tags}" loading="lazy" width="280" height="180"/>
+    
+<div class="info">
+  <p class="info-item"> <b>Likes</b>
+    <span>${likes}</span>
+  </p>
+  <p class="info-item"><b>Views</b> 
+    <span>${views}</span>
+  </p>
+  <p class="info-item">
+    <b>Comments</b>
+    <span>${comments}</span>
+  </p>
+  <p class="info-item">
+    <b>Downloads</b>
+    <span>${downloads}</span>
+  </p>
+</div>
+</div></a>`;
+    }
+    ).join('');
+
+
+    ref.gallery.insertAdjacentHTML("beforeend", img);
+    lightbox.refresh();
+  }
+
+  function clearList() {
+    ref.gallery.innerHTML = "";
+  }
+function onError(err) {
+    console.log (err);
+    observer.unobserve(ref.guard);
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+}
+function notification(res) {
+  if (galleryApiService.page === 2)
+Notiflix.Notify.success(`Hooray! We found ${res.totalHits} images.`);
+else if (res.hits.length < galleryApiService.perPage) {
+  observer.unobserve(ref.guard);
+  Notiflix.Notify.warning(
+    "We're sorry, but you've reached the end of search results."
+  );
+} 
+}
+const scroll = {
+root: null,
+rootHeight: '300px',
+
+};
+const observer = new IntersectionObserver(onScroll , scroll);
+
+function onScroll(enteries) {
+    console.log(enteries);
+    enteries.forEach(entry => {
+        console.log(entry.isIntersecting);
+        if (entry.isIntersecting) {
+            fetchArticles()
+        }
+    });
+}
